79ada0222730763cac3c34437db653844655a9a9
mmuellersoppart <mmuellersoppart@gmail.com>
2022-12-26 17:08:38 -0600

    P0001

---
+++ src/P0001/P0001.jl
@@ -0,0 +1,5 @@
+# derived here - https://cocalc.com/share/public_paths/6354379129ee472a62b3aed5659b261e157daca9
+
+p1(n) = 3/2 * (floor(1/3 * n) + 1) * floor(1/3 * n) + 5/2 * (floor(1/5 * n) + 1) * floor(1/5 * n) - 15/2 * (floor(1/15 * n) + 1) * floor(1/15 * n)
+
+p1(999)
